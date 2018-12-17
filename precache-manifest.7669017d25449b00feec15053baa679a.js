9340652f6d4558a97c04d1ef3b0713209ceb5ad5
Wilhelm Picaco <wpicaco@gmail.com>
2018-12-16 18:51:11 -0700

    Updates

---
+++ precache-manifest.7669017d25449b00feec15053baa679a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66c34b29d278a5d14e76",
+    "url": "/memory-game/static/js/runtime~main.66c34b29.js"
+  },
+  {
+    "revision": "9bf2349bf06573bafd5f",
+    "url": "/memory-game/static/js/main.9bf2349b.chunk.js"
+  },
+  {
+    "revision": "3b0a40bde90379538364",
+    "url": "/memory-game/static/js/1.3b0a40bd.chunk.js"
+  },
+  {
+    "revision": "9bf2349bf06573bafd5f",
+    "url": "/memory-game/static/css/main.a4a7a5be.chunk.css"
+  },
+  {
+    "revision": "36b8bb7c430359914752aac2f02eb4a1",
+    "url": "/memory-game/index.html"
+  }
+];
