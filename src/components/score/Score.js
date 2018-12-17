91a397d891e7dc63de05bccb763e09d6d0335cb8
Wilhelm Picaco <wpicaco@gmail.com>
2018-12-16 18:31:07 -0700

    style stuff

--- src/components/score/Score.js
+++ src/components/score/Score.js
@@ -1,14 +1,20 @@
 import React from "react";
-import {Card} from "react-materialize";
+import {Card, CardTitle} from "react-materialize";
 import "./Score.css";
 
 const Score = props => {
 
     return (
-        <Card id="ScoreCard" className='blue-grey darken-1' textClassName='white-text'>
-            <h2 id="score">
-                Score: {props.state.score}
-            </h2>
+        <Card 
+            horizontal header={
+                <CardTitle 
+                    image="images/logo.png"
+                    className="scoreCard"
+                >
+                </CardTitle>
+            } 
+        >
+            <h2>Score: {props.state.score}</h2>
         </Card>
     );
 };
